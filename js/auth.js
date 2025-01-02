a4bfa3a3dfd8903f1a3b012d13416fac3e811b35
PrinceKes <princeade5565@gmail.com>
2025-01-02 04:52:52 -0800

    update all

--- js/auth.js
+++ js/auth.js
@@ -1,15 +1,12 @@
 window.addEventListener('load', () => {
-  // Check for user ID in localStorage
   const userId = localStorage.getItem('user_id') || localStorage.getItem('User_id');
 
   if (!userId) {
-      // Redirect user to the bot for authentication if ID is missing
       alert('User ID is missing. Please log in again.');
       window.location.href = 'https://t.me/EazyNaijaPayBot';
   } else {
       console.log(`User ID loaded: ${userId}`);
-      // Save the consistent key format back to localStorage
-      localStorage.setItem('user_id', userId); // Normalize to 'user_id'
+      localStorage.setItem('user_id', userId);
   }
 });
 
